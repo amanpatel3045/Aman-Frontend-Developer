0c33477ae49cf05206800ab14e498c85e1480bd2
amanpatel3045 <amanpatel3045@gmail.com>
2023-06-03 03:58:29 +0530

    Poster component completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Topbar from "./components/Topbar/Topbar";
+import Poster from "./components/Poster/Poster";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <Poster/>
     </div>
   );
 }
