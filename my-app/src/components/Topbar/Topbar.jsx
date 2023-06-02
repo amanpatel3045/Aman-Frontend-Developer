24c889124e1f62a97a357f854da43f0f25cdaeb1
amanpatel3045 <amanpatel3045@gmail.com>
2023-06-03 03:23:13 +0530

    first commit after creating react app

---
+++ my-app/src/components/Topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <div>
+      <div>
+        <h4>SpaceX</h4>
+        <h4>Capusle</h4>
+        <h4>Signup</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
